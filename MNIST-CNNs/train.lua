5674bd39e6e79ec93bd6f66c747edcc79ec17ded
Paco Zamora Martinez <pakozm@gmail.com>
2014-07-24 15:00:34 +0200

    Updated licenses

--- MNIST-CNNs/train.lua
+++ MNIST-CNNs/train.lua
@@ -1,3 +1,21 @@
+--[[
+  MNIST-CNNs APRIL-ANN tutorial
+  Copyright (C) 2014  Francisco Zamora-Martinez
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+
 local bunch_size    = 64
 local learning_rate = 0.0524
 local momentum      = 0.0157
